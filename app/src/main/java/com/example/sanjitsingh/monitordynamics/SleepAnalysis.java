dd9b1031a41483b7aeffd3df2761a9131d89f0f2
Sanjit Singh <sanjitpsingh@yahoo.com>
2018-12-02 03:39:32 -0500

    Integrated Wolfram API for sleep analysis

--- app/src/main/java/com/example/sanjitsingh/monitordynamics/SleepAnalysis.java
+++ app/src/main/java/com/example/sanjitsingh/monitordynamics/SleepAnalysis.java
@@ -1,18 +1,24 @@
 package com.example.sanjitsingh.monitordynamics;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.support.customtabs.CustomTabsIntent;
 
-public class SleepAnalysis extends AppCompatActivity implements View.OnClickListener  {
+public class SleepAnalysis extends AppCompatActivity  {
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sleep_analysis);
-    }
 
-    @Override
-    public void onClick(View view) {
-
+        String url = "https://www.wolframcloud.com/objects/b4fb5fc8-c189-4d1f-add8-8dfec66b8521";
+        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+        // set toolbar color and/or setting custom actions before invoking build()
+        // Once ready, call CustomTabsIntent.Builder.build() to create a CustomTabsIntent
+        CustomTabsIntent customTabsIntent = builder.build();
+        // and launch the desired Url with CustomTabsIntent.launchUrl()
+        customTabsIntent.launchUrl(this, Uri.parse(url));
     }
 }
