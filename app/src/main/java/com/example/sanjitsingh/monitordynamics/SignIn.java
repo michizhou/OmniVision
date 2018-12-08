13bf5afc2d8470ca71eff77491afa5b5b572d98a
Sanjit Singh <sanjitpsingh@yahoo.com>
2018-12-08 16:16:39 -0500

    Got FaceBook Login Integrated

---
+++ app/src/main/java/com/example/sanjitsingh/monitordynamics/SignIn.java
@@ -0,0 +1,13 @@
+package com.example.sanjitsingh.monitordynamics;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SignIn extends AppCompatActivity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in);
+    }
+
+}
